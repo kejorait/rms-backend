85b837be1a6e6ad62d2444c828d7bc2590a62a16
mliem2k <michael.liem2k@gmail.com>
2024-11-02 15:23:46 +0800

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='admin')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve_react_app(path):
+    # Check if the requested path exists as a file in the 'dashboard' folder
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        # For any other route, serve 'index.html' for the SPA
+        return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=6000)
